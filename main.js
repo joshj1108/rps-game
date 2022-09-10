3accc6a74afa7c1229b67d9c7332c05130f7b429
JoshJ1108 <joshua.a.jackson.1990@gmail.com>
2022-09-09 21:37:36 -0700

    Allow multiple rounds, score not working

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function getRandom(list) {
 
 
 //player/computer selection variables
-const playerSelection = 'RoCK'
+const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 
 console.log(playerSelection);
@@ -20,16 +20,22 @@ console.log(computerSelection);
 //function plays single round of RPS, comparing playerSelection() to computerSelection() and returns a result
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
+        playerScore++;
         return ("You win! Rock beats Scissors");
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+        computerScore++;
         return ("You lose, Paper beats Rock");
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+        playerScore++;
         return ("You win! Paper beats Rock");
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
+        computerScore++;
         return ("You lose, Scissors beat Paper");
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+        playerScore++;
         return ("You win! Scissors beat Paper");
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+        computerScore++;
         return ("You lose, Rock beats Scissors");
     } else if (playerSelection.toLowerCase() === computerSelection){
         return ("It\s a tie");
@@ -39,6 +45,24 @@ function playRound(playerSelection, computerSelection) {
 }
 
 //output result of playRound() to console
-console.log(playRound(playerSelection, computerSelection));
+//console.log(playRound(playerSelection, computerSelection))
+let playerScore = 0;
+let computerScore = 0;
 
-//function game() calls playRound() for five rounds/keeps score and reports winner/loser at end
+//function game() calls playround() play 5 rounds/keep score/report winner at end
+function game () {
+       
+    for (let i = 0; i < 5; i++){
+            playRound(playerSelection, computerSelection)
+        } 
+
+        if (playerScore > computerScore){
+            return "Congrats, you win!"
+        } else {
+            return "You lose, better luck next time"
+        }
+}
+
+console.log(game());
+console.log(computerScore);
+console.log(playerScore);
